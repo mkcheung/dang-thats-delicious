fde284b3f6c8a0c7b6434038edb33617816b2aa0
Mars Cheung <mars.kwong.cheung@gmail.com>
2017-11-19 01:04:13 -0800

    add demonstrations of mixins and path to add store as well as add store controller method

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -8,3 +8,10 @@ exports.homePage = (req, res) =>{
 	res.render('index');
 };
 
+exports.addStore = (req, res) =>{
+	// res.send('It works');
+	res.render('editStore', {
+		title: 'Add Store'
+	});
+};
+
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ const storeController = require('../controllers/storeController')
 // Do work here
 // router.get('/', storeController.myMiddleware, storeController.homePage);
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
 
 module.exports = router;
