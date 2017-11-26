b14f1672a4430044c64db72913baac3f68f3fae9
Mars Cheung <mars.kwong.cheung@gmail.com>
2017-11-26 15:13:32 -0800

    add password reset

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
-const authController = require('../controllers/authController')
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 // router.get('/', storeController.myMiddleware, storeController.homePage);
@@ -37,6 +37,12 @@ router.get('/logout', authController.logout);
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/account', userController.account);
-router,post('/account', catchErrors(userController.updateAccount));
+router.post('/account', catchErrors(userController.updateAccount));
+router.post('/account/forgot', catchErrors(authController.forgot));
+router.get('/account/reset/:token', catchErrors(authController.reset));
+router.post('/account/reset/:token', 
+	authController.confirmedPasswords,
+	catchErrors(authController.update)
+);
 
 module.exports = router;
